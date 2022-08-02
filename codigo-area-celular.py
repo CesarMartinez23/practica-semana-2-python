647c2826c77a4d8b8b36ce94721319186ae78442
CesarMartinez23 <oliver-cesar@hotmail.com>
2022-08-01 21:05:40 -0600

    Exercise1

---
+++ codigo-area-celular.py
@@ -0,0 +1,7 @@
+# Programa que pide el código de área y número de teléfono y luego imprime en pantalla el número completo.
+
+postalCode = input("Ingrese el codigo de area: ")
+
+phoneNumber = input("Ingrese el numero de telefono: ")
+
+print("El numero completo es:", postalCode + " " + phoneNumber)
